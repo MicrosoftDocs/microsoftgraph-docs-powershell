e16126811fa94b5475fe04ea5b027830464b12c1
Timothy Wamalwa <10947120+timayabi2020@users.noreply.github.com>
2022-09-14 12:54:01 +0300

    Added a step for module generation

---
+++ scripts/GenerateServiceModules.ps1
@@ -0,0 +1,14 @@
+#This script takes advantage of autorest funcitonality for copying examples to the main md files during module generation.
+Set-Location msgraph-sdk-powershell
+$date = Get-Date -Format "dd-MM-yyyy"
+$proposedBranch = "weekly_autorest_rerun_"+$date
+$exists = git branch -l $proposedBranch
+if ([string]::IsNullOrEmpty($exists)) {
+    git checkout -b $proposedBranch
+}else{
+	Write-Host "Branch already exists"
+     git checkout $proposedBranch
+}
+
+.\tools\GenerateModules.ps1 -Build
+
