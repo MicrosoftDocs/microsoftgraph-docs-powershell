c78de4f2bab969663eeb9ed026db2f56eb7fbfbc
Timothy Wamalwa <timwamalwa@gmail.com>
2023-06-29 17:49:52 +0300

    Updated help file

--- microsoftgraph/UpdateHelp.ps1
+++ microsoftgraph/UpdateHelp.ps1
@@ -80,15 +80,17 @@ function Update-GraphModuleHelp {
         [ValidateNotNullOrEmpty()]
         [string] $ModulePrefix = "Microsoft.Graph"
     )
-    if($GraphProfile -eq "beta"){
-        $ModulePrefix = "Microsoft.Graph.Beta"
-    }
+    
     $moduleImportName = "$ModulePrefix.$ModuleName"
-    $moduleDocsPath = Join-Path $PSScriptRoot ".\$GraphProfilePath\$moduleImportName"
+    $Path = "$ModulePrefix.$ModuleName"
+    if($GraphProfile -eq 'beta'){
+       $moduleImportName = "$ModulePrefix.Beta.$ModuleName"
+       $Path = "$ModulePrefix.Beta.$ModuleName"
+    }
+    $moduleDocsPath = Join-Path $PSScriptRoot ".\$GraphProfilePath\$Path"
     $logsPath = Join-Path $PSScriptRoot ".\logs\$moduleImportName-$GraphProfile.txt"
 
-    Import-Module $moduleImportName -Force -Global
-    Select-MgProfile $GraphProfile
+    Import-Module $moduleImportName -RequiredVersion 2.0.0 -Force -Global
     Update-Help -ModuleDocsPath $moduleDocsPath -LogsPath $logsPath
 }
 
