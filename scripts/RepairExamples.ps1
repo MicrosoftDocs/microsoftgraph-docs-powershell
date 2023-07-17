615f0f678b7a7360374552f72886a22c0a52d1f4
Timothy Wamalwa <timwamalwa@gmail.com>
2023-07-17 16:03:55 +0300

    Rectified branch name error

--- scripts/RepairExamples.ps1
+++ scripts/RepairExamples.ps1
@@ -167,6 +167,7 @@ function Repair-Examples{
 }
 
 Set-Location microsoftgraph-docs-powershell
+$date = Get-Date -Format "dd-MM-yyyy"
 $proposedBranch = "weekly_v2_docs_update_$date"
 $exists = git branch -l $proposedBranch
 if ([string]::IsNullOrEmpty($exists)) {
