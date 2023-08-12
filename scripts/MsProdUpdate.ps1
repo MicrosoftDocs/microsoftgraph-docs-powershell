32ecd36c4fcb50c0fb6905245bee373b5709d2c6
Timothy Wamalwa <timwamalwa@gmail.com>
2023-08-12 23:00:45 +0300

    Uncommented set location

--- scripts/MsProdUpdate.ps1
+++ scripts/MsProdUpdate.ps1
@@ -298,7 +298,7 @@ if ($ModulesToGenerate.Count -eq 0) {
     [HashTable] $ModuleMapping = Get-Content $ModuleMappingConfigPath | ConvertFrom-Json -AsHashTable
     $ModulesToGenerate = $ModuleMapping.Keys
 }
-#Set-Location ..\microsoftgraph-docs-powershell
+Set-Location ..\microsoftgraph-docs-powershell
 Write-Host -ForegroundColor Green "-------------finished checking out to today's branch-------------"
 Start-Generator -ModulesToGenerate $ModulesToGenerate
 Write-Host -ForegroundColor Green "-------------Done-------------"
