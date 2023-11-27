6311688ab41a6d0b3a87dec86ce28a455fe695e9
Timothy Wamalwa <timwamalwa@gmail.com>
2023-11-27 16:16:39 +0300

    Deleted unnecessary folder

--- scripts/RepairExamplesDescriptionsAndTitle.ps1
+++ scripts/RepairExamplesDescriptionsAndTitle.ps1
@@ -9,7 +9,7 @@ Param(
 function Get-GraphMapping {
     $graphMapping = @{}
     $graphMapping.Add("v1.0", "v1.0")
-    $graphMapping.Add("beta", "beta")
+    #$graphMapping.Add("beta", "beta")
     return $graphMapping
 }
 
@@ -28,10 +28,10 @@ function Start-Copy {
 		}
         Get-FilesByProfile -GraphProfile $graphProfile -GraphProfilePath $profilePath -ModulePrefix $ModulePrefix -ModulesToGenerate $ModulesToGenerate 
     }
-    git config --global user.email "timwamalwa@gmail.com"
-    git config --global user.name "Timothy Wamalwa"
-    git add .
-    git commit -m "Corrected titles" 
+    # git config --global user.email "timwamalwa@gmail.com"
+    # git config --global user.name "Timothy Wamalwa"
+    # git add .
+    # git commit -m "Corrected titles" 
 }
 function Get-FilesByProfile{
  Param(
@@ -104,6 +104,7 @@ function Import-Descriptions {
         }
         $End++  
     }
+    Write-Host $File
     Get-ExistingDescriptions -Content $Content -File $File  -start 0 -end $End -NoOfExamples $NoOfExamples
     if(Test-Path $File){
     $TitleCount = 1
@@ -123,11 +124,11 @@ function Import-Descriptions {
         $Extracted = $Matches[0]
         $FinalOutput = "## EXAMPLES`r`n$RetainedContent`r`n## PARAMETERS"
         $text = $DestinationContent.ToString()
-        if(-not($Extracted.Contains("``````powershell"))){
-            $text = $text.Replace($Extracted, "## PARAMETERS") 
-            Write-Host "Does not have snippet"
-        }else{
+        if(($Extracted.Contains("``````powershell")) -or ($Extracted.Contains("### EXAMPLE"))){
             $text = $text.Replace($Extracted, $FinalOutput)
+        }else{
+            $text = $text.Replace($Extracted, "## PARAMETERS")
+            Write-Host "Does not have snippet"
         }
           $text | Out-File $File -Encoding UTF8
      }
@@ -210,16 +211,16 @@ function Get-ExistingDescriptions {
     }
    
 }
-Set-Location microsoftgraph-docs-powershell
-$date = Get-Date -Format "dd-MM-yyyy"
-$proposedBranch = "weekly_v2_docs_update_$date"
-$exists = git branch -l $proposedBranch
-if ([string]::IsNullOrEmpty($exists)) {
-    git checkout -b $proposedBranch
-}else{
-	Write-Host "Branch already exists"
-     git checkout $proposedBranch
-}
+# Set-Location microsoftgraph-docs-powershell
+# $date = Get-Date -Format "dd-MM-yyyy"
+# $proposedBranch = "weekly_v2_docs_update_$date"
+# $exists = git branch -l $proposedBranch
+# if ([string]::IsNullOrEmpty($exists)) {
+#     git checkout -b $proposedBranch
+# }else{
+# 	Write-Host "Branch already exists"
+#      git checkout $proposedBranch
+# }
 if (-not (Test-Path $ModuleMappingConfigPath)) {
     Write-Error "Module mapping file not be found: $ModuleMappingConfigPath."
 }
@@ -227,7 +228,7 @@ if ($ModulesToGenerate.Count -eq 0) {
     [HashTable] $ModuleMapping = Get-Content $ModuleMappingConfigPath | ConvertFrom-Json -AsHashTable
     $ModulesToGenerate = $ModuleMapping.Keys
 }
-Set-Location ..\microsoftgraph-docs-powershell
+#Set-Location ..\microsoftgraph-docs-powershell
 Write-Host -ForegroundColor Green "-------------finished checking out to today's branch-------------"
 Start-Copy -ModulesToGenerate $ModulesToGenerate
 Write-Host -ForegroundColor Green "-------------Done-------------"
