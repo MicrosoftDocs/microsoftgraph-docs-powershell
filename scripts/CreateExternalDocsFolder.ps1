b7d596116dd56ca9ebf9852d3507c811710ed090
Timothy Wamalwa <timwamalwa@gmail.com>
2024-04-02 18:51:00 +0300

    Updated paths

--- scripts/CreateExternalDocsFolder.ps1
+++ scripts/CreateExternalDocsFolder.ps1
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 Param(
-    [string] $MissingMsProdHeaderPath = (Join-Path $PSScriptRoot "../missingexternaldocsurl")
+    [string] $MissingMsProdHeaderPath = (Join-Path $PSScriptRoot "missingexternaldocsurl")
 )
 if (-not(Test-Path -PathType Container $MissingMsProdHeaderPath)) {
     New-Item -ItemType Directory -Force -Path $MissingMsProdHeaderPath
