6932201cd63a795baaee45dff4ef7111ae673e07
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2024-07-17 22:03:53 +0300

    Refactored examples and descriptions repair script

--- scripts/RepairExamplesDescriptionsAndTitle.ps1
+++ scripts/RepairExamplesDescriptionsAndTitle.ps1
@@ -2,57 +2,49 @@
 # Licensed under the MIT License.
 Param(
     $ModulesToGenerate = @(),
-    [string] $ModuleMappingConfigPath = (Join-Path $PSScriptRoot "../msgraph-sdk-powershell/config/ModulesMapping.jsonc"),
-	[string] $SDKDocsPath = (Join-Path $PSScriptRoot "../msgraph-sdk-powershell/src"),
-	[string] $WorkLoadDocsPath =  (Join-Path $PSScriptRoot "../microsoftgraph")
+    [string] $SDKDocsPath = (Join-Path $PSScriptRoot "../msgraph-sdk-powershell/src"),
+    [string] $WorkLoadDocsPath = (Join-Path $PSScriptRoot "../microsoftgraph"),
+    [string] $CommandMetadataPath = (Join-Path $PSScriptRoot "../msgraph-sdk-powershell/src/Authentication/Authentication/custom/common/MgCommandMetadata.json")
 )
-function Get-GraphMapping {
-    $graphMapping = @{}
-    $graphMapping.Add("v1.0", "v1.0")
-    $graphMapping.Add("beta", "beta")
-    return $graphMapping
-}
 
 function Start-Copy {
-    Param(
-        $ModulesToGenerate = @()
-    )
 
+    
     $ModulePrefix = "Microsoft.Graph"
-    $GraphMapping = Get-GraphMapping 
-    $GraphMapping.Keys | ForEach-Object {
-        $graphProfile = $_
-		$profilePath = "graph-powershell-1.0"
-		if($graphProfile -eq "beta"){
-			$profilePath = "graph-powershell-beta"
-		}
-        Get-FilesByProfile -GraphProfile $graphProfile -GraphProfilePath $profilePath -ModulePrefix $ModulePrefix -ModulesToGenerate $ModulesToGenerate 
-    }
-    git config --global user.email "GraphTooling@service.microsoft.com"
-    git config --global user.name "Microsoft Graph DevX Tooling"
-    git add .
-    git commit -m "Corrected titles descriptions and examples" 
-}
-function Get-FilesByProfile{
- Param(
-        [ValidateSet("beta", "v1.0")]
-        [string] $GraphProfile = "v1.0",
-        [ValidateNotNullOrEmpty()]
-        [string] $GraphProfilePath = "graph-powershell-1.0",
-        [ValidateNotNullOrEmpty()]
-        [string] $ModulePrefix = "Microsoft.Graph",
-        [ValidateNotNullOrEmpty()]
-        $ModulesToGenerate = @()
-    )
 
-    $ModulesToGenerate | ForEach-Object {
-        $ModuleName = $_
-		$docs = Join-Path $SDKDocsPath $ModuleName $GraphProfile "examples"
-        Copy-Files -DocPath $docs -GraphProfilePath $GraphProfilePath -Module $ModuleName -ModulePrefix $ModulePrefix -GraphProfile $GraphProfile
-    }
+    if (Test-Path $CommandMetadataPath) {
+        $CommandMetadataContent = Get-Content $CommandMetadataPath | ConvertFrom-Json
+        $CommandMetadataContent | ForEach-Object {
+            $ModuleName = $_.Module
+            $GraphProfile = $_.ApiVersion
+            $Command = $_.Command
+            $GraphProfilePath = "graph-powershell-1.0"
+            if ($GraphProfile -eq "beta") {
+                $GraphProfilePath = "graph-powershell-beta"
+                $ModulePrefix = "Microsoft.Graph.Beta"
+                $ModuleName = $ModuleName.Replace("Beta.", "")
+            }
+            $DocPath = Join-Path $SDKDocsPath $ModuleName $GraphProfile "examples" "$Command.md"
+            try {
+                Copy-Files -DocPath $DocPath -GraphProfilePath $GraphProfilePath -Module $ModuleName -ModulePrefix $ModulePrefix -GraphProfile $GraphProfile -Command $Command
+            }
+            catch {
+                Write-Host "`nError Message: " $_.Exception.Message
+                Write-Host "`nError in Line: " $_.InvocationInfo.Line
+                Write-Host "`nError in Line Number: "$_.InvocationInfo.ScriptLineNumber
+                Write-Host "`nError Item Name: "$_.Exception.ItemName
+            }
+        
+        }
+        git config --global user.email "GraphTooling@service.microsoft.com"
+        git config --global user.name "Microsoft Graph DevX Tooling"
+        git add .
+        git commit -m "Repaired examples and descriptions" 
 
+    }
+    
 }
-function Copy-Files{
+function Copy-Files {
     param(
         [ValidateSet("beta", "v1.0")]
         [string] $GraphProfile = "v1.0",
@@ -62,28 +54,45 @@ function Copy-Files{
         [string] $Module = "Users",
         [ValidateNotNullOrEmpty()]
         [string] $ModulePrefix = "Microsoft.Graph",
-		[ValidateNotNullOrEmpty()]
-        [string] $DocPath = "..\msgraph-sdk-powershell\src\Users\v1.0\examples"
+        [ValidateNotNullOrEmpty()]
+        [string] $DocPath = "..\msgraph-sdk-powershell\src\Users\v1.0\examples\Get-MgUser.md",
+        [ValidateNotNullOrEmpty()]
+        [string] $Command = "Get-MgUser"
     )
-    $Path = "$ModulePrefix.$ModuleName"
-    if($GraphProfile -eq 'beta'){
-       $Path = "$ModulePrefix.Beta.$ModuleName"
-    }
-     $Destination = Join-Path $WorkLoadDocsPath $GraphProfilePath $Path
-
-     if ((Test-Path $DocPath)) {
-       
-        foreach ($File in Get-ChildItem $DocPath) {
+    try {
+        $Path = "$ModulePrefix.$ModuleName"
+        $DestinationFile = Join-Path $WorkLoadDocsPath $GraphProfilePath $Path "$Command.md"
+        if ((Test-Path $DocPath)) {
             # Read the content of the file searching for example headers.
-            $Command = [System.IO.Path]::GetFileName($File)
-            $content = Get-Content -Path $File
-            $DestinationFile = Join-Path $Destination $Command
-            $Content = Get-Content -Path $File
-            Import-Descriptions -Content $Content -File $DestinationFile
-           
+            $EmptyFile = Test-FileEmpty $DocPath
+                
+            if (Test-Path $DestinationFile) {
+                if ($EmptyFile) {
+                    #For removing existing wrong examples and descriptions
+                    Remove-WrongExamples -File $DestinationFile
+                }
+                else {
+                    $Content = Get-Content -Path $DocPath
+                    Import-Descriptions -Content $Content -File $DestinationFile
+                }
+            }
         }
- 
-    }     
+    
+    }
+    catch { 
+        Write-Host "`nError Message: " $_.Exception.Message
+        Write-Host "`nError in Line: " $_.InvocationInfo.Line
+        Write-Host "`nError in Line Number: "$_.InvocationInfo.ScriptLineNumber
+        Write-Host "`nError Item Name: "$_.Exception.ItemName
+    }
+}
+
+function Test-FileEmpty {
+
+    Param ([Parameter(Mandatory = $true)][string]$File)
+  
+    if ((Test-Path -LiteralPath $File) -and !((Get-Content -LiteralPath $File -Raw) -match '\S')) { return $true } else { return $false }
+  
 }
 function Import-Descriptions {
     Param (
@@ -106,62 +115,61 @@ function Import-Descriptions {
     }
     Write-Host $File
     Get-ExistingDescriptions -Content $Content -File $File  -start 0 -end $End -NoOfExamples $NoOfExamples
-    if(Test-Path $File){
-    $TitleCount = 1
-    $DestinationContent = Get-Content -Encoding UTF8 -Raw $File
-    $RetainedContent = $null
-    foreach ($Ex in $RetainedExamples) {
+    if (Test-Path $File) {
+        $TitleCount = 1
+        $DestinationContent = Get-Content -Encoding UTF8 -Raw $File
+        $RetainedContent = $null
+        foreach ($Ex in $RetainedExamples) {
             $ContentBody = $Ex.Split("**##@**")[0]
             $ContentDescription = $Ex.Split("**##@**")[2]
             $RetainedContent += "$ContentBody$ContentDescription"  
             $TitleCount++ 
                         
-    }
+        }
 
    
-    if(-not($Null -eq $RetainedContent) -and -not($RetainedContent.Contains("Add title here"))){
-     if($DestinationContent -match $Re){
-        $Extracted = $Matches[0]
-        $FinalOutput = "## EXAMPLES`r`n$RetainedContent`r`n## PARAMETERS"
-        $text = $DestinationContent.ToString()
-        if(($Extracted.Contains("``````powershell")) -or ($Extracted.Contains("### EXAMPLE"))){
-            $text = $text.Replace($Extracted, $FinalOutput)
-        }else{
-            $text = $text.Replace($Extracted, "## PARAMETERS")
-            Write-Host "Does not have snippet"
-        }
-          $text | Out-File $File -Encoding UTF8
-     }
-     if($DestinationContent -match $Re2){
-        $Extracted2 = $Matches[0]
-        $DescriptionCommand = [System.IO.Path]::GetFileNameWithoutExtension($File)
-        $Description1 = "This example shows how to use the $DescriptionCommand Cmdlet."
-        $Description2 = "To learn about permissions for this resource, see the [permissions reference](/graph/permissions-reference)."
+        if (-not($Null -eq $RetainedContent) -and -not($RetainedContent.Contains("Add title here"))) {
+            if ($DestinationContent -match $Re) {
+                $Extracted = $Matches[0]
+                $FinalOutput = "## EXAMPLES`r`n$RetainedContent`r`n## PARAMETERS"
+                $text = $DestinationContent.ToString()
+                if (($Extracted.Contains("``````powershell")) -or ($Extracted.Contains("### EXAMPLE"))) {
+                    $text = $text.Replace($Extracted, $FinalOutput)
+                }
+                else {
+                    $text = $text.Replace($Extracted, "## PARAMETERS")
+                    Write-Host "Does not have snippet"
+                }
+                $text | Out-File $File -Encoding UTF8
+            }
+            if ($DestinationContent -match $Re2) {
+                $Extracted2 = $Matches[0]
+                $DescriptionCommand = [System.IO.Path]::GetFileNameWithoutExtension($File)
+                $Description1 = "This example shows how to use the $DescriptionCommand Cmdlet."
+                $Description2 = "To learn about permissions for this resource, see the [permissions reference](/graph/permissions-reference)."
       
-        $text2 = $DestinationContent.ToString()
-        if(-not($Extracted2.Contains("## EXAMPLES"))){
-            Write-Host "Does not have snippet $DescriptionCommand"
-            $text2 = $text2.Replace($Description1, $null)
-            $text2 = $text2.Replace($Description2, $null)
-            $text2 | Out-File $File -Encoding UTF8
+                $text2 = $DestinationContent.ToString()
+                if (-not($Extracted2.Contains("## EXAMPLES"))) {
+                    Write-Host "Does not have snippet $DescriptionCommand"
+                    $text2 = $text2.Replace($Description1, $null)
+                    $text2 = $text2.Replace($Description2, $null)
+                    $text2 | Out-File $File -Encoding UTF8
 
-        }
-     }
-     $Stream = [IO.File]::OpenWrite($File)
-     try
-     {
-         $Stream.SetLength($stream.Length - 2)
-         $Stream.Close()
-     }
-     catch
-     {
+                }
+            }
+            $Stream = [IO.File]::OpenWrite($File)
+            try {
+                $Stream.SetLength($stream.Length - 2)
+                $Stream.Close()
+            }
+            catch {
          
-     }
-     $Stream.Dispose()
-     $RetainedExamples.Clear()
-    }
+            }
+            $Stream.Dispose()
+            $RetainedExamples.Clear()
+        }
     
-}
+    }
     
 }
 function Get-ExistingDescriptions {
@@ -211,14 +219,16 @@ function Get-ExistingDescriptions {
     }
    
 }
-if (-not (Test-Path $ModuleMappingConfigPath)) {
-    Write-Error "Module mapping file not be found: $ModuleMappingConfigPath."
-}
-if ($ModulesToGenerate.Count -eq 0) {
-    [HashTable] $ModuleMapping = Get-Content $ModuleMappingConfigPath | ConvertFrom-Json -AsHashTable
-    $ModulesToGenerate = $ModuleMapping.Keys
+function Remove-WrongExamples {
+    Param(
+        [string]$File
+    )
+    $DestinationContent = Get-Content -Encoding UTF8 -Raw $File
+    $DestinationContent = $DestinationContent -replace "## EXAMPLES(?s).*## PARAMETERS", "## PARAMETERS"
+    $DestinationContent | Out-File $File -Encoding UTF8
 }
 
 Write-Host -ForegroundColor Green "-------------finished checking out to today's branch-------------"
-Start-Copy -ModulesToGenerate $ModulesToGenerate
+Start-Copy
+
 Write-Host -ForegroundColor Green "-------------Done-------------"
