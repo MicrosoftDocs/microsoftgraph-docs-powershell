7b4e275c7bbf2d03f71b914f2bbe83d8b92dfd9b
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2025-01-24 10:35:39 +0300

    Added try catch

--- scripts/GeneratePermissionsTable.ps1
+++ scripts/GeneratePermissionsTable.ps1
@@ -38,8 +38,15 @@ function Start-Generator {
             #Check if all types of permissions in the their respective arrays are empty. If empty just skip the command
             if ($DelegatedWorkPermissions.Count -eq 0 -and $ApplicationPermissions.Count -eq 0 -and $DelegatedPersonalPermissions.Count -eq 0) {
                 Write-Host "Skipping $CommandName as it does not have any permissions";
-            }else{
-                New-ReferenceTable -CommandName $CommandName -DelegatedWorkPermissions $DelegatedWorkPermissions -ApplicationPermissions $ApplicationPermissions -DelegatedPersonalPermissions $DelegatedPersonalPermissions -ApiVersion $ApiVersion -Module $Module; 
+            }
+            else {
+                try {
+                    New-ReferenceTable -CommandName $CommandName -DelegatedWorkPermissions $DelegatedWorkPermissions -ApplicationPermissions $ApplicationPermissions -DelegatedPersonalPermissions $DelegatedPersonalPermissions -ApiVersion $ApiVersion -Module $Module; 
+                }
+                catch {
+                    Write-Host "Error in $CommandName";
+                }
+                
             }
         } 
         $CmdList += $CommandName;  
@@ -63,46 +70,47 @@ function New-ReferenceTable {
         [string]$ApiVersion = "v1.0",
         [string]$Module = "Users"
     )
-    if ($CmdList -notcontains $CommandName) {
+    try {
+        if ($CmdList -notcontains $CommandName) {
 
-        $MdFile = "Microsoft.Graph.$Module/$CommandName.md";
+            $MdFile = "Microsoft.Graph.$Module/$CommandName.md";
         
-        $File = (Join-Path $PSScriptRoot "../microsoftgraph/graph-powershell-1.0/$MdFile");
-        if ($ApiVersion -eq "beta") {
-            $File = (Join-Path $PSScriptRoot "../microsoftgraph/graph-powershell-beta/$MdFile");
-        }
-        if (Test-Path $File) {
-            $DWPerms = ""
-            $AppPerms = ""
-            $DPPerms = ""
-            if ($DelegatedWorkPermissions.Count -gt 0) {
-                $DelegatedWorkPermissions | ForEach-Object {
-                    $DWPerms += $_ + ", ";
+            $File = (Join-Path $PSScriptRoot "../microsoftgraph/graph-powershell-1.0/$MdFile");
+            if ($ApiVersion -eq "beta") {
+                $File = (Join-Path $PSScriptRoot "../microsoftgraph/graph-powershell-beta/$MdFile");
+            }
+            if (Test-Path $File) {
+                $DWPerms = ""
+                $AppPerms = ""
+                $DPPerms = ""
+                if ($DelegatedWorkPermissions.Count -gt 0) {
+                    $DelegatedWorkPermissions | ForEach-Object {
+                        $DWPerms += $_ + ", ";
+                    }
                 }
-            }
-            else {
-                $DWPerms = "Not supported";
-            }
+                else {
+                    $DWPerms = "Not supported";
+                }
     
-            if ($ApplicationPermissions.Count -gt 0) {
-                $ApplicationPermissions | ForEach-Object {
-                    $AppPerms += $_ + ", "; 
+                if ($ApplicationPermissions.Count -gt 0) {
+                    $ApplicationPermissions | ForEach-Object {
+                        $AppPerms += $_ + ", "; 
+                    }
                 }
-            }
-            else {
-                $AppPerms = "Not supported";
-            }
-            if ($DelegatedPersonalPermissions.Count -gt 0) {
-                $DelegatedPersonalPermissions | ForEach-Object {
-                    $DPPerms += $_ + ", ";  
+                else {
+                    $AppPerms = "Not supported";
                 }
-            }
-            else {
-                $DPPerms = "Not supported";
-            }
+                if ($DelegatedPersonalPermissions.Count -gt 0) {
+                    $DelegatedPersonalPermissions | ForEach-Object {
+                        $DPPerms += $_ + ", ";  
+                    }
+                }
+                else {
+                    $DPPerms = "Not supported";
+                }
 
-            #Generate a markdown table
-            $markdownTable = @"
+                #Generate a markdown table
+                $markdownTable = @"
 | Permission type | Permissions (from least to most privileged) |
 | --------------- | ------------------------------------------  |
 | Delegated (work or school account) | $DWPerms |
@@ -110,51 +118,55 @@ function New-ReferenceTable {
 | Application | $AppPerms |
 "@;
 
-            $V1_PermissionsFolder = (Join-Path $PSScriptRoot "../microsoftgraph/graph-powershell-1.0/permissions/$CommandName.md")
-            $Beta_PermissionsFolder = (Join-Path $PSScriptRoot "../microsoftgraph/graph-powershell-beta/permissions/$CommandName.md")
+                $V1_PermissionsFolder = (Join-Path $PSScriptRoot "../microsoftgraph/graph-powershell-1.0/permissions/$CommandName.md")
+                $Beta_PermissionsFolder = (Join-Path $PSScriptRoot "../microsoftgraph/graph-powershell-beta/permissions/$CommandName.md")
 
-            $IncludeLink = "[!INCLUDE [permissions-table](../permissions/$CommandName.md)]"
-            if ($ApiVersion -eq "v1.0") {
-                if (-not (Test-Path $V1_PermissionsFolder)) {
-                    New-Item -ItemType file -Path $V1_PermissionsFolder -Force
-                }
-                else {
-                    #Clear content first before writing
-                    Clear-Content $V1_PermissionsFolder
-                }
-                #Write the content
-                Set-Content -Path $V1_PermissionsFolder -Value $markdownTable
-
-            }
-            else {
-                if (-not (Test-Path $Beta_PermissionsFolder)) {
-                    New-Item -ItemType file -Path $Beta_PermissionsFolder -Force
-                }
-                else {
-                    #Clear content first before writing
-                    Clear-Content $Beta_PermissionsFolder
+                $IncludeLink = "[!INCLUDE [permissions-table](../permissions/$CommandName.md)]"
+                if ($ApiVersion -eq "v1.0") {
+                    if (-not (Test-Path $V1_PermissionsFolder)) {
+                        New-Item -ItemType file -Path $V1_PermissionsFolder -Force
+                    }
+                    else {
+                        #Clear content first before writing
+                        Clear-Content $V1_PermissionsFolder
+                    }
                     #Write the content
-                } 
-                Set-Content -Path $Beta_PermissionsFolder -Value $markdownTable
-            }
+                    Set-Content -Path $V1_PermissionsFolder -Value $markdownTable
+
+                }
+                else {
+                    if (-not (Test-Path $Beta_PermissionsFolder)) {
+                        New-Item -ItemType file -Path $Beta_PermissionsFolder -Force
+                    }
+                    else {
+                        #Clear content first before writing
+                        Clear-Content $Beta_PermissionsFolder
+                        #Write the content
+                    } 
+                    Set-Content -Path $Beta_PermissionsFolder -Value $markdownTable
+                }
 
 
-            if ((Get-Content -Raw -Path $File) -match '(## DESCRIPTION)[\s\S]*## EXAMPLES') {
-                $Link = "**Permissions**`r`n$IncludeLink`r`n`n## EXAMPLES"
+                if ((Get-Content -Raw -Path $File) -match '(## DESCRIPTION)[\s\S]*## EXAMPLES') {
+                    $Link = "**Permissions**`r`n$IncludeLink`r`n`n## EXAMPLES"
     (Get-Content $File) | 
-                Foreach-Object { $_ -replace '## EXAMPLES', $Link }  | 
-                Out-File $File
-            }
-            else {
-                $Link = "**Permissions**`r`n$markdownTable`r`n`n## PARAMETERS"
+                    Foreach-Object { $_ -replace '## EXAMPLES', $Link }  | 
+                    Out-File $File
+                }
+                else {
+                    $Link = "**Permissions**`r`n$markdownTable`r`n`n## PARAMETERS"
     (Get-Content $File) | 
-                Foreach-Object { $_ -replace '## PARAMETERS', $Link }  | 
-                Out-File $File
+                    Foreach-Object { $_ -replace '## PARAMETERS', $Link }  | 
+                    Out-File $File
+                }
             }
+
         }
+    }
+    catch {
+        Write-Host "Error in $CommandName";
 
     }
-
 }
 
 Start-Generator;
