1c86ac85aefd73925cba2f0b22e44df7a588bbea
Timothy Wamalwa <timwamalwa@gmail.com>
2025-09-04 17:01:07 +0300

    Uncommented installation of powerhtml

--- scripts/MsSubserviceUpdate.ps1
+++ scripts/MsSubserviceUpdate.ps1
@@ -120,13 +120,13 @@ function FetchStream {
     return ($ReadStream, $HttpWebResponse)
 }
 
-# if (!(Get-Module "powershell-yaml" -ListAvailable -ErrorAction SilentlyContinue)) {
-#     Install-Module "powershell-yaml" -AcceptLicense -Scope CurrentUser -Force
-# }
-# If (-not (Get-Module -ErrorAction Ignore -ListAvailable PowerHTML)) {
-#     Write-Verbose "Installing PowerHTML module for the current user..."
-#     Install-Module PowerHTML -ErrorAction Stop -Scope CurrentUser -Force
-# }
+if (!(Get-Module "powershell-yaml" -ListAvailable -ErrorAction SilentlyContinue)) {
+    Install-Module "powershell-yaml" -AcceptLicense -Scope CurrentUser -Force
+}
+If (-not (Get-Module -ErrorAction Ignore -ListAvailable PowerHTML)) {
+    Write-Verbose "Installing PowerHTML module for the current user..."
+    Install-Module PowerHTML -ErrorAction Stop -Scope CurrentUser -Force
+}
 Import-Module -ErrorAction Stop PowerHTML
 Write-Host -ForegroundColor Green "-------------finished checking out to today's branch-------------"
 Start-Generator
